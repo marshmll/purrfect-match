4e22660d3c3f40aed22c7c88b2d8a39d0800f801
marshmll <renandasilvaoliveiraaandrade@gmail.com>
2024-09-08 17:30:09 -0300

    start auth

--- php/auth.php
+++ php/auth.php
@@ -1,5 +1,40 @@
 <?php
-    $username = $_POST['username'];
+    $url = "http://127.0.0.1:8000/auth";
 
-    echo $username;
+    $data = [
+        'grant_type' => '',
+        'username' => $_POST['username'],
+        'password' => $_POST['password'],
+        'scope' => '',
+        'client_id' => '',
+        'client_secret' => '',
+    ];
+    
+    $options = [
+        'http' => [
+            'header'=> [
+                'accept: application/json',
+                'Content-Type: application/x-www-form-urlencoded',
+            ],
+            'method' => 'POST',
+            'content' => http_build_query($data),
+        ],
+    ];
+    
+    try {
+
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+
+        if ($result == false) {
+            die("erro");
+        }
+    }
+    catch (Exception $e) {
+        echo "erro http";
+    }
+
+    $token = json_decode($result);
+
+    var_dump($token);
 ?>
