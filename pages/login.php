2a2c777e3f5cf54107bdc866251917aa48d99c18
marshmll <renandasilvaoliveiraandrade@gmail.com>
2024-09-08 16:13:51 -0300

    current files

--- pages/login.php
+++ pages/login.php
@@ -6,6 +6,14 @@
     <title>Login | Purrfect Match</title>
 </head>
 <body>
-    
+    <main>
+        <form action="../php/auth.php" method="POST">
+            <label for="username">Usuário</label>
+            <input type="text" name="username" id="username">
+            <label for="password">Senha</label>
+            <input type="password" name="password" id="password">
+            <input type="submit" value="Entrar">
+        </form>
+    </main>
 </body>
 </html>
