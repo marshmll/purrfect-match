f9b2bd88d4c0728ad66b8408079f6f88699ec41f
Ricardo-LK <rilucaslk@gmail.com>
2024-10-27 09:39:35 -0300

    merge

--- api/content/rescues/rescue.php
+++ api/content/rescues/rescue.php
@@ -6,9 +6,6 @@ header('Content-Type: application/json');
 
 $headers = apache_request_headers();
 if (
-    !isset($body['name']) or
-    !isset($body['email']) or
-    !isset($body['phone']) or
     !isset($body['cep']) or
     !isset($body['state']) or
     !isset($body['city']) or
@@ -22,20 +19,22 @@ if (
 try {
     Database::beginTransaction();
 
-    $insertQuery = "
-        INSERT INTO rescues (name, email, phone, cep, state, city, neighborhood, street, characteristics, observations)
-        VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
-    ";
+    $rescue = Database::query(
+        "SELECT * FROM rescues WHERE name = '%s'",
+        [$body['name']]
+    );
+
+    $rescue_id = $rescue['id'];
 
     $result = Database::query(
-        "INSERT INTO rescues (user_id, status, closure_datetime, addr_city, addr_state, addr_street, addr_number, addr_zipcode, characteristics, description)
+        "INSERT INTO rescues (user_id, status, addr_city, addr_state, addr_street, addr_number, addr_zipcode, characteristics, description)
         VALUES ('%d', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')",
         [
-            $body['name'],
-            $body['email'],
-            $body['phone'],
-            $body['cep'],
+            $rescue_id,
+            "pendente",
+            $body['city'],
             $body['state'],
+            $body['street'],
             $body['city'],
             $body['neighborhood'],
             $body['street'],
