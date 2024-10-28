bacf989ac399fd9bc07bda419b15d1fae22d2842
marshmll <renandasilvaoliveiraandrade@gmail.com>
2024-10-28 18:32:48 -0300

    rescues now working properly

--- api/content/rescues/all.php
+++ api/content/rescues/all.php
@@ -1,12 +1,27 @@
 <?php
 require_once('../../utils/database.php');
 require_once('../../utils/http_responses.php');
+require_once('../../utils/jwt.php');
 require_once('../../utils/check_authentication.php');
 
 header('Content-Type: application/json');
 
 $headers = apache_request_headers();
 checkUserAuthentication($headers);
+
+// Remove 'Bearer ' from the token
+$token = getAuthTokenFromHeaders($headers);
+
+$jwt = new JWTManager(SECRET_KEY);
+
+// Check if the token has the expected signature
+if (!$jwt->isTokenValid($token) or $jwt->isTokenExpired($token))
+    sendNotAuthenticatedResponse();
+
+$payload = $jwt->decodeToken($token);
+
+if (!in_array($payload['rol'], ['root', 'supervisor', 'manager']))
+    sendResponse(json_encode(['detail' => 'O usuário não tem permissões suficientes.']), HttpStatus::Unauthorized->value);
 
 try {
     // Fetch rescues from the database
