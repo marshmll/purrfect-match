3ea1db1268781f1602aac27ce3e7bc8847ae6352
marshmll <renandasilvaoliveiraandrade@gmail.com>
2024-10-28 21:03:00 -0300

    chats query improvement

--- api/content/users/chats/contacts.php
+++ api/content/users/chats/contacts.php
@@ -22,31 +22,33 @@ $payload = $jwt->decodeToken($token);
 // Get all users that sent any message to the current user and get the last
 // sent message in the chat.
 $contacts = Database::query(
-    "SELECT users.id,
-            users.name,
-            users.username,
-            users.pfp_url,
-            messages.sender_id,
-            messages.receiver_id,
-            messages.content AS last_message_content,
-            messages.status AS last_message_status,
-            messages.sent_datetime AS last_message_datetime
-         FROM messages
-         JOIN (
-            SELECT 
-                LEAST(sender_id, receiver_id) AS user1,
-                GREATEST(sender_id, receiver_id) AS user2,
-                MAX(sent_datetime) AS last_message_datetime
-            FROM messages
-            WHERE sender_id = %d OR receiver_id = %d
-            GROUP BY user1, user2
-         ) AS latest_messages
-         ON (LEAST(messages.sender_id, messages.receiver_id) = latest_messages.user1
-             AND GREATEST(messages.sender_id, messages.receiver_id) = latest_messages.user2
-             AND messages.sent_datetime = latest_messages.last_message_datetime)
-         JOIN users
-         ON users.id = IF(messages.sender_id = %d, messages.receiver_id, messages.sender_id)
-         ORDER BY messages.sent_datetime DESC",
+    "WITH latest_messages AS (
+        SELECT 
+            LEAST(sender_id, receiver_id) AS user1,
+            GREATEST(sender_id, receiver_id) AS user2,
+            MAX(sent_datetime) AS last_message_datetime
+        FROM messages
+        WHERE sender_id = %d OR receiver_id = %d
+        GROUP BY user1, user2
+    )
+    SELECT 
+        u.id,
+        u.name,
+        u.username,
+        u.pfp_url,
+        m.sender_id,
+        m.receiver_id,
+        m.content AS last_message_content,
+        m.status AS last_message_status,
+        m.sent_datetime AS last_message_datetime
+    FROM messages m
+    JOIN latest_messages lm ON (
+        LEAST(m.sender_id, m.receiver_id) = lm.user1
+        AND GREATEST(m.sender_id, m.receiver_id) = lm.user2
+        AND m.sent_datetime = lm.last_message_datetime
+    )
+    JOIN users u ON u.id = IF(m.sender_id = %d, m.receiver_id, m.sender_id)
+    ORDER BY m.sent_datetime DESC",
     [$payload['sub'], $payload['sub'], $payload['sub']],
     true
 );
