5521a4f139338a579be091d4c499344e9c611482
Ricardo-LK <rilucaslk@gmail.com>
2024-10-31 16:03:38 -0300

    Index.html fixed

--- public/scripts/content/index.js
+++ public/scripts/content/index.js
@@ -11,16 +11,18 @@ async function renderIndexPage() {
 
     const catsCards = cats.map(cat => {
         return `
-            <li class="card">
-                <article class="card">
-                <a class="card__image image-skeleton"></a>
-                <button class="card__fav text-skeleton" title="Adicionar aos favoritos"></button>
-                <div class="card__text">
-                    <h3 class="card__name text-skeleton"></h3>
-                    <span class="card__age text-skeleton"></span>
+             <li class="card">
+                <div class="img">
+                    <img
+                        id="img"
+                        src="${cat.picture_url}"
+                        alt="cat"
+                        draggable="false"
+                    />
+
+                    <h2 class="cat-name">${cat.name}</h2>
+                    <span>${cat.agr}</span>
                 </div>
-                <div class="card__personalities text-skeleton"></div>
-            </article>
             </li>
         `;
     }).join("");
